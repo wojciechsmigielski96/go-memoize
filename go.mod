3d9140b9f2fb81a38381a11725641dfb8452bef5
Wojciech Smigielski <wsmigielski@rippling.com>
2022-12-27 12:53:51 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module wojciechsmigielski96/go-memoize
+
+
+go 1.19
+
+require (
+	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/smartystreets/assertions v1.2.0
+	github.com/smartystreets/gunit v1.4.2
+	golang.org/x/sync v0.0.0-20220907140024-f12130a52804
+)
---
+++ memoize_test.go
@@ -0,0 +1,85 @@
+package memoize
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/assertions"
+	"github.com/smartystreets/gunit"
+)
+
+func TestSuite(t *testing.T) {
+	gunit.Run(new(F), t)
+}
+
+type F struct {
+	*gunit.Fixture
+}
+
+// TestBasic adopts the code from readme.md into a simple test case
+func (t *F) TestBasic() {
+	expensiveCalls := 0
+
+	// Function tracks how many times its been called
+	expensive := func() (interface{}, error) {
+		expensiveCalls++
+		return expensiveCalls, nil
+	}
+
+	cache := NewMemoizer(90*time.Second, 10*time.Minute)
+
+	// First call SHOULD NOT be cached
+	result, err, cached := cache.Memoize("key1", expensive)
+	t.So(err, ShouldBeNil)
+	t.So(result.(int), ShouldEqual, 1)
+	t.So(cached, ShouldBeFalse)
+
+	// Second call on same key SHOULD be cached
+	result, err, cached = cache.Memoize("key1", expensive)
+	t.So(err, ShouldBeNil)
+	t.So(result.(int), ShouldEqual, 1)
+	t.So(cached, ShouldBeTrue)
+
+	// First call on a new key SHOULD NOT be cached
+	result, err, cached = cache.Memoize("key2", expensive)
+	t.So(err, ShouldBeNil)
+	t.So(result.(int), ShouldEqual, 2)
+	t.So(cached, ShouldBeFalse)
+}
+
+// TestFailure checks that failed function values are not cached
+func (t *F) TestFailure() {
+	calls := 0
+
+	// This function will fail IFF it has not been called before.
+	twoForTheMoney := func() (interface{}, error) {
+		calls++
+
+		if calls == 1 {
+			return calls, errors.New("Try again")
+		} else {
+			return calls, nil
+		}
+	}
+
+	cache := NewMemoizer(90*time.Second, 10*time.Minute)
+
+	// First call should fail, and not be cached
+	result, err, cached := cache.Memoize("key1", twoForTheMoney)
+	t.So(err, ShouldNotBeNil)
+	t.So(result.(int), ShouldEqual, 1)
+	t.So(cached, ShouldBeFalse)
+
+	// Second call should succeed, and not be cached
+	result, err, cached = cache.Memoize("key1", twoForTheMoney)
+	t.So(err, ShouldBeNil)
+	t.So(result.(int), ShouldEqual, 2)
+	t.So(cached, ShouldBeFalse)
+
+	// Third call should succeed, and be cached
+	result, err, cached = cache.Memoize("key1", twoForTheMoney)
+	t.So(err, ShouldBeNil)
+	t.So(result.(int), ShouldEqual, 2)
+	t.So(cached, ShouldBeTrue)
+}
